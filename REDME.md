cf39c5081d914a67523b351a3915dd46d94e80d7
TetianaOgnivenko <tatianaognivenko@gmail.com>
2023-08-24 16:06:51 +0200

    delete comment code

--- REDME.md
+++ REDME.md
@@ -1,4 +1,4 @@
-https://ibb.co/7RtJYpp
-https://ibb.co/d6BBN8v
-https://ibb.co/sg9GqbX
-https://ibb.co/V3pt5Dh
+https://ibb.co/DwGrvcB
+https://ibb.co/7JVXdpd
+https://ibb.co/M7p3W1t
+https://ibb.co/02xgtc2
--- index.js
+++ index.js
@@ -1,13 +1,5 @@
 const contacts = require("./contacts")
-// const listContacts = require("./contacts.js")
-// const yargs = require("yargs")
-// const {hideBin} = require("yargs/helpers")
-
-// const { Command } = require('commander');
-// const program = new Command();
 const {program} = require("commander")
-
-
 
 const invokeAction = async({action, id, name, email, phone}) =>{
     switch(action){
@@ -17,44 +9,25 @@ const invokeAction = async({action, id, name, email, phone}) =>{
             break
         case "get":
             const contact = await contacts.getContactById(id)
-            console.log(contact)
+            console.table(contact)
             break
         case "add":
             const newContact = await contacts.addContact({name, email, phone})
-            console.log(newContact)
+            console.table(newContact)
             break
         case "updateById":
             const renewContact = await contacts.updateContact(id, {name, email, phone})
-            console.log(renewContact)
+            console.table(renewContact)
             break
         case "remove":
             const deleteContact = await contacts.removeContact(id)
-            console.log(deleteContact)
+            console.table(deleteContact)
             break 
             default:
                 console.warn('\x1B[31m Unknown action type!');
     }
    
 }
-// invokeAction({action: "read"})
-// invokeAction({action: "getById", id: "qdggE76Jtbfd9eWJHrssH"})
-// invokeAction({action: "add", name: "Tanya", email: "tatianaognivenko@gmail.com", phone: "3-13-86"})
-// invokeAction({action: "updateById", id: "pLr0e5FVrxepO8MfF5Y5z", name: "Tetiana", email: "tetianaognivenko@gmail.com", phone: "3-13-86"})
-// invokeAction({action: "deleteById", id: "VeBQn-VFAh9oMpDSnXnzN"})
-// console.log(process.argv)
-
-// const actionIndex = process.argv.indexOf("--action")
-// if (actionIndex !== -1){
-//     const action = process.argv[actionIndex + 1]
-//     console.log("my action", action)
-//     invokeAction({action})
-// }
-
-// const arr = hideBin(process.argv)
-// console.log(arr)
-// const {argv} = yargs(arr)
-// console.log(argv)
-
 
 program
   .option('-a, --action <type>', 'choose action')
